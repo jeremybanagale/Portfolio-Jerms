81e43746e9ab990e849e09edcdcf743b97aad54d
jeremybanagale <jbanagale.bos@gmail.com>
2017-05-04 16:01:48 +0800

    initial commit

---
+++ scripts/data.js
@@ -0,0 +1,90 @@
+var profile = {
+  info: {
+    first_name: "Jeremy Earl",
+    middle_name: "Aclan",
+    last_name: "Bañagale",
+    title: "Web Developer",
+    age: "20",
+    location: "San Francisco Calihan San Pablo, Laguna",
+    mobile: "+639568878987",
+    email: "jbanagale.bos@gmail.com",
+    company: "Big Outsource",
+    company_location: "San Pablo City, Laguna",
+    company_title: 'OJT On-the-Job Training',
+    period: 'January 2017 - May 2017',
+    icon: 'imgs/BOS.png'
+  },
+  social: {
+    facebook: "",
+    linkedin: "",
+    github: "https://github.com/jeremybanagale",
+    codepen: "http://codepen.io/exiege/",
+    stack_overflow: "http://stackoverflow.com/users/4918036/jigno-alfred-venezuela"
+  }
+};
+
+var skills = [{
+  name: "Git",
+  url: 'https://git-scm.com/'
+}, {
+  name: "Data Extraction",
+  url: 'https://en.wikipedia.org/wiki/Data_extraction'
+}, {
+  name: "Pug.js",
+  url: 'https://pugjs.org/api/getting-started.html'
+}, {
+  name: "Express.js",
+  url: 'https://expressjs.com/'
+}, {
+  name: "Node.js",
+  url: 'https://expressjs.com/'
+}, {
+  name: "Angular.js",
+  url: 'https://angularjs.org/'
+}, {
+  name: "SASS",
+  url: 'http://sass-lang.com/'
+}, {
+  name: "Stylus",
+  url: 'http://stylus-lang.com/'
+}];
+
+var schools = [{
+  name: "Laguna State Polytechnic University",
+  icon: 'imgs/lspu.jpg',
+  sy: "2013-2017"
+}, {
+  name: "CREST Christian School",
+  icon: '',
+  sy: "2009-2013"
+}, {
+  name: "Don Enrique Bautista Elementary School",
+  icon: 'imgs/debes.jpg',
+  sy: "2006-2009"
+}, {
+  name: "San Pablo Central School",
+  icon: 'imgs/central.png',
+  sy: "2006-2009"
+}];
+
+// var languages = [{
+//   name: "PHP",
+//   percent: "60%",
+//   offset: '351'
+// }, {
+//   name: "VB.NET",
+//   percent: "75%",
+//   offset: '481'
+// }, {
+//   name: "Python",
+//   percent: "45%",
+//   offset: '281'
+// }, {
+//   name: "Javascript",
+//   percent: "80%",
+//   offset: '521'
+// }, {
+//   name: "HTML/CSS",
+//   percent: "58%",
+//   offset: '321'
+// }];
